9cb2d303840e3e1845d0e6527d7e20260dcf62da
liujianjia <liujianjia@jd.com>
2025-03-29 23:46:31 +0800

    change

--- backend/demo/main.py
+++ backend/demo/main.py
@@ -4,9 +4,17 @@ from backend.llm.openai import query_openai
 from backend.util.file import read_file, write_file
 
 # 原文
-# story = read_file("story.txt")
-# print(f"原文：{story}")
-# print("============================================================")
+story = read_file("story.txt")
+print(f"原文：{story}")
+print("============================================================")
+
+# 角色提取
+input = story
+sys1 = read_file("prompt0.txt")
+openai = query_openai(input, sys1, 0.01)
+write_file(openai, "result0.txt")
+print(openai)
+print("============================================================")
 
 # 拆分动态镜头
 # input = story
@@ -17,13 +25,13 @@ from backend.util.file import read_file, write_file
 # print("============================================================")
 
 # 优化提示词
-result1 = read_file("result1.txt")
-loads = json.loads(result1)
-
-for storyboardObj in loads:
-    print({storyboardObj["text"]})
-
-    storyboardArr = storyboardObj["storyboard"]
-    for storyboard in storyboardArr:
-        print("\t", {storyboard["storyboard_text"]})
-        print("\t", {storyboard["prompts"]})
+# result1 = read_file("result1.txt")
+# loads = json.loads(result1)
+#
+# for storyboardObj in loads:
+#     print({storyboardObj["text"]})
+#
+#     storyboardArr = storyboardObj["storyboard"]
+#     for storyboard in storyboardArr:
+#         print("\t", {storyboard["storyboard_text"]})
+#         print("\t", {storyboard["prompts"]})
